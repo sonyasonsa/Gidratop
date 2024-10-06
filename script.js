60281d12f17fbbe00dbe3eb7e9aa4d8c786ab25e
sonyasonsa <sokolova.s98@mail.ru>
2024-10-06 21:55:34 +0700

    добавила слайдер с акциями

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 {
-  var swiper = new Swiper(".swiper", {
+  var swiper1 = new Swiper(".swiper", {
     loop: true,
     navigation: {
       nextEl: ".swiper-button-next",
@@ -7,8 +7,25 @@
     },
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
-    mousewheel: true,
     keyboard: true,
+  });
+}
+
+{
+  var swiper2 = new Swiper(".sale-banner", {
+    slidesPerView: 3,
+    slidesPerGroup: 1,
+    loop: true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
 }
