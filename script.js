82222832de869dd278789c05c25eb5147adbbd9f
sonyasonsa <sokolova.s98@mail.ru>
2024-10-12 22:48:13 +0700

    добавила блок с рекламой

--- script.js
+++ script.js
@@ -14,9 +14,9 @@
 }
 
 {
-  var swiper2 = new Swiper(".sale-banner", {
-    slidesPerView: 3,
-    slidesPerGroup: 1,
+  var swiper3 = new Swiper(".help-banner", {
+    slidesPerView: 6,
+
     loop: true,
     spaceBetween: 30,
     pagination: {
@@ -29,3 +29,40 @@
     },
   });
 }
+
+const checkWidth = () => {
+  let width = window.innerWidth;
+  if (width > 1200) {
+    var swiper2 = new Swiper(".sale-banner", {
+      slidesPerView: 3,
+      slidesPerGroup: 1,
+      loop: true,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  } else {
+    var swiper2 = new Swiper(".sale-banner", {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+      loop: true,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
+};
+checkWidth();
+window.addEventListener("resize", checkWidth);
